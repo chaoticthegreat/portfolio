69cd8641025381bdd244970fffad2e96afe64b1a
Ryan Abraham <ryan.ryanabraham@gmail.com>
2024-07-01 20:10:25 -0700

    fixed logo not loading

--- src/lib/components/site/icons/logo.svelte
+++ src/lib/components/site/icons/logo.svelte
@@ -1,1 +1,4 @@
-<img src="/src/lib/components/site/icons/logo.png" alt="logo" width="100" height="25" />
+<script>
+	import logo from "$lib/components/site/icons/logo.png"
+</script>
+<img src={logo} alt="logo" width="100" height="25" />
